a00d77ddbde75b9b210237e8d9e4923f5c34421a
amit-nadcab <amitnadcab@gmail.com>
2023-02-13 18:05:09 +0530

    update

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Footer } from '../Components/Footer'
+import { Navbar } from '../Components/Navbar'
 
 export const Signup = () => {
   return (
     <>
+        <Navbar/>
+            
         <Footer/>
     </>
   )

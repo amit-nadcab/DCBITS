f3c8da7bdecba0091170ac50816b4f0a478f7e48
amit-nadcab <amitnadcab@gmail.com>
2023-02-16 22:22:49 +0530

    fix login api

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,95 +1,158 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 // import { useDispatch } from "react-redux";
-import { Footer } from '../Components/Footer'
-import { Navbar } from '../Components/Navbar'
-import {RiLockPasswordFill} from 'react-icons/ri'
-import {MdEmail} from 'react-icons/md'
-import { Link, useNavigate } from 'react-router-dom'
-import { login } from '../utils/apiFunction'
-
+import { Footer } from "../Components/Footer";
+import { Navbar } from "../Components/Navbar";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { MdEmail } from "react-icons/md";
+import { Link, useNavigate } from "react-router-dom";
+import { login, resendOtp } from "../utils/apiFunction";
+import { toast } from "react-toastify";
 
 export const Login = () => {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   // const dispatch = useDispatch()
-  const [formData, setFormdata] = useState({})
-  const [showEmailError, setShowEmailError] = useState(false)
-  const [passwordError, setShowPasswordError] = useState(false)
+  const [formData, setFormdata] = useState({});
+  const [showEmailError, setShowEmailError] = useState(false);
+  const [passwordError, setShowPasswordError] = useState(false);
 
-  const getInputs = (value,name)=>{
-    const data = {[name]: value}
-    setFormdata({...formData, ...data})
-  }
+  const getInputs = (value, name) => {
+    const data = { [name]: value };
+    setFormdata({ ...formData, ...data });
+  };
 
   return (
-   <>
-    <Navbar />
-   
+    <>
+      <Navbar />
+
       <div className=" mx-auto signup-form-page page-wrapper">
-      <div className=" mt-5 mb-5">
-        <div className="container">
-            <div className='col-md-4 signup-form mx-auto'>
-            <div className="header">
-            <h1>Login from here</h1>
+        <div className=" mt-5 mb-5">
+          <div className="container">
+            <div className="col-md-4 signup-form mx-auto">
+              <div className="header">
+                <h1>Login from here</h1>
+              </div>
+              <form>
+                <div className="input">
+                  <i className="">
+                    <MdEmail />
+                  </i>
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                    onChange={(event) => {
+                      getInputs(event.target.value, event.target.name);
+                    }}
+                    onBlur={() => {
+                      const re =
+                        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                      const t = re.test(formData?.email);
+                      if (t) {
+                        setShowEmailError(false);
+                      } else {
+                        setShowEmailError(true);
+                      }
+                    }}
+                  />
+                  {showEmailError ? (
+                    <p className="text-danger text-start">Enter Valid Email</p>
+                  ) : null}
+                </div>
+                <div className="input">
+                  <i className="">
+                    <RiLockPasswordFill />
+                  </i>
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    onChange={(event) => {
+                      getInputs(event.target.value, event.target.name);
+                    }}
+                    onBlur={() => {
+                      if (
+                        formData?.password === "" ||
+                        formData?.password === undefined
+                      ) {
+                        setShowPasswordError(true);
+                      } else {
+                        setShowPasswordError(false);
+                      }
+                    }}
+                  />
+                  {passwordError ? (
+                    <p className="text-danger text-start">Enter Password</p>
+                  ) : null}
+                </div>
+                <input
+                  className="signup-btn"
+                  type="button"
+                  value="LOGIN"
+                  onClick={() => {
+                    if (!passwordError && !showEmailError) {
+                      login(formData?.email, formData?.password, navigate).then(
+                        (res) => {
+                          if (
+                            res.message === "Login successfully!" &&
+                            res?.params?.ev == 1
+                          ) {
+                            navigate("/dashboard", {
+                              state: { user_id: res.params.user_id },
+                            });
+                            toast(res.message);
+                          } else if (
+                            res.message === "Login successfully!" &&
+                            res?.params?.ev === false
+                          ) {
+                            resendOtp(res?.params?.user_id)
+                              .then((res1) => {
+                                console.log(res1, "resend res");
+                                if (
+                                  res1?.message === "Email sent successfully" &&
+                                  res1?.status == 200
+                                ) {
+                                  navigate("/verifyEmail", {
+                                    state: {
+                                      user_id: res?.params?.user_id,
+                                    },
+                                  });
+                                }
+                              })
+                              .catch((err) => {
+                                console.log(err);
+                              });
+                          }
+                        }
+                      );
+                    }
+                    if (
+                      formData?.password === "" ||
+                      formData?.password === undefined
+                    ) {
+                      setShowPasswordError(true);
+                    } else {
+                      setShowPasswordError(false);
+                    }
+                    if (
+                      formData?.email === "" ||
+                      formData?.email === undefined
+                    ) {
+                      setShowEmailError(true);
+                    } else {
+                      setShowEmailError(false);
+                    }
+                  }}
+                />
+              </form>
+              <p>
+                Don't have an account? <Link to="/signup">sign up</Link>
+              </p>
+            </div>
           </div>
-          <form>
-            <div className="input">
-              <i className=""><MdEmail/></i>
-              <input type="email" placeholder="Email" name='email' onChange={(event)=>{getInputs(event.target.value, event.target.name)}}
-              onBlur={() => {
-                const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                const t = re.test(formData?.email)
-                if (t) {
-                  setShowEmailError(false)
-                } else {
-                  setShowEmailError(true)
-                }
-              }}
-              />
-              {showEmailError ? <p className='text-danger text-start'>Enter Valid Email</p> : null}
-            </div>
-            <div className="input">
-              <i className=""><RiLockPasswordFill/></i>
-              <input type="password" placeholder="Password" name='password' onChange={(event)=>{getInputs(event.target.value, event.target.name)}}
-                onBlur={()=>{
-                    if(formData?.password === '' || formData?.password === undefined){
-                      setShowPasswordError(true)
-                    }
-                    else{
-                      setShowPasswordError(false)
-                    }
-                }}
-               
-              />
-               {passwordError ? <p className='text-danger text-start'>Enter Password</p> : null}
-            </div>
-            <input className="signup-btn" type="button" value="LOGIN" onClick={()=>{
-                      if(!passwordError && !showEmailError){
-                        login(formData?.email, formData?.password,navigate)
-                      }
-                      if(formData?.password === '' || formData?.password === undefined){
-                        setShowPasswordError(true)
-                      }
-                      else{
-                        setShowPasswordError(false)
-                      }
-                      if(formData?.email === '' || formData?.email === undefined){
-                        setShowEmailError(true)
-                      }
-                      else{
-                        setShowEmailError(false)
-                      }
-            }}/>
-          </form>
-          <p>Don't have an account? <Link to="/signup">sign up</Link></p>
-            </div>
-          
         </div>
       </div>
-      </div>
-   
 
       <Footer />
-   </>
-  )
-}
+    </>
+  );
+};
--- src/utils/apiFunction.js
+++ src/utils/apiFunction.js
@@ -3,7 +3,6 @@ import { BASE_URL } from "./config";
 
 import { toast } from "react-toastify";
 
-
 export const registration = async (
   refID,
   email,
@@ -12,28 +11,35 @@ export const registration = async (
   navigate
 ) => {
   try {
-    const data = await axios.post(`${BASE_URL}/register-user`, {"parent_ref-code": refID,'email': email,'password': password,'confirm_password': confirm_password});
-    console.log(data.data,"reg response");
+    const data = await axios.post(`${BASE_URL}/register-user`, {
+      "parent_ref-code": refID,
+      email: email,
+      password: password,
+      confirm_password: confirm_password,
+    });
+    console.log(data.data, "reg response");
     if (data.data.status == "400") {
       toast.error(data.data.message);
     }
     return data.data;
   } catch (error) {
-    if(error)
-    {
-      toast.error(error?.response?.data?.message)
+    if (error) {
+      toast.error(error?.response?.data?.message);
     }
-    console.log(error,"error");
+    console.log(error, "error");
   }
 };
 
-export const verifyEmail = async (otp, user_id,navigate) => {
+export const verifyEmail = async (otp, user_id, navigate) => {
   try {
-    const data = await axios.post(`${BASE_URL}/varifie/email`, { otp: otp, user_id: user_id, });
+    const data = await axios.post(`${BASE_URL}/varifie/email`, {
+      otp: otp,
+      user_id: user_id,
+    });
     console.log(data.data.params.ev, "verify Response");
-    if(data.data.params.ev){
-      toast.info(data.data.message)
-      navigate('/dashboard')
+    if (data.data.params.ev) {
+      toast.info(data.data.message);
+      navigate("/dashboard");
     }
     return data;
   } catch (error) {
@@ -42,45 +48,38 @@ export const verifyEmail = async (otp, user_id,navigate) => {
   }
 };
 
-export const login = async (email, password,navigate) => {
+export const login = async (email, password, navigate) => {
   try {
-    const data = await axios.post(`${BASE_URL}/login`, { email: email, password: password })
+    const data = await axios.post(`${BASE_URL}/login`, {
+      email: email,
+      password: password,
+    });
+    return data.data;
+  } catch (error) {
+    toast.error(error.response.data.message);
+  }
+};
+
+export const resendOtp = async (user_id) => {
+  try {
+    const data = await axios.post(`${BASE_URL}/resend-otp`, {
+      user_id: user_id,
+    });
     console.log(data.data);
-    if(data.data.message === 'Login successfully!' && data?.data?.params?.ev == 1){
-      navigate('/dashboard',{state: {user_id: data.data.params.user_id}})
-      toast(data.data.message)
-    }else if(data.data.message === 'Login successfully!' && data?.data?.params?.ev === false){
-        resendOtp(data?.data?.params?.user_id).then((res)=>{
-          console.log(res,"resend res");
-          if(res?.message === 'Email sent successfully' && res?.status == 200){
-            navigate('/verifyEmail',{state:{user_id: data?.data?.params?.user_id}})
-          }
-        }).catch((err)=>{
-          console.log(err);
-        })
-    }
-    return data.data
-  } catch (error) {
-    toast.error(error.response.data.message)
-  }
-}
-
-export const resendOtp = async(user_id)=>{
-  try {
-      const data = await axios.post(`${BASE_URL}/resend-otp`,{user_id: user_id})
-      console.log(data.data);
-      return data.data
+    return data.data;
   } catch (error) {
     console.log(error);
   }
-}
+};
 
-export const createWallet = async(user_id)=>{
+export const createWallet = async (user_id) => {
   try {
-      const data = await axios.post(`${BASE_URL}/create-wallets-dcbit`,{user_id: user_id})
-      console.log(data.data);
-      return data.data
+    const data = await axios.post(`${BASE_URL}/create-wallets-dcbit`, {
+      user_id: user_id,
+    });
+    console.log(data.data);
+    return data.data;
   } catch (error) {
     console.log(error);
   }
-}
+};
